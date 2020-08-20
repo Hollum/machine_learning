9162ff1f5c583accd5a9bfe221648289fa963229
Hollum <hollum@hotmail.com>
2020-08-20 22:40:39 +0200

    changed epochs and learning rate

--- O1 - Linear Regression/task1.py
+++ O1 - Linear Regression/task1.py
@@ -4,18 +4,11 @@ import pandas as pd
 
 # Visualize result
 data = pd.read_csv("./data/length_weight.csv")
-# Preview the first 5 lines of the loaded data
 
-# for index, row in data.iterrows():
-#    length = row.values[0]
-#   weight = row.values[1]
-#  print("length =", length, "weight =", weight)
-
-
-# Numpy: length = data["length"].to_numpy()
 weight = data["weight"].tolist()
 length = data["length"].tolist()
-#print(weight)
+
+# Numpy: length = data["length"].to_numpy()
 
 # Observed/training input and output
 x_train = torch.tensor(length).reshape(-1, 1)  # x_train = [[1], [1.5], [2], [3], [4], [5], [6]]
@@ -25,7 +18,7 @@ y_train = torch.tensor(weight).reshape(-1, 1)  # y_train = [[5], [3.5], [3], [4]
 class LinearRegressionModel:
     def __init__(self):
         # Model variables
-        self.W = torch.tensor([[0.0]], requires_grad=True)  # requires_grad enables calculation of gradients
+        self.W = torch.tensor([[0.5]], requires_grad=True)  # requires_grad enables calculation of gradients
         self.b = torch.tensor([[0.0]], requires_grad=True)
 
     # Predictor
@@ -34,21 +27,15 @@ class LinearRegressionModel:
 
     # Uses Mean Squared Error
     def loss(self, x, y):
-        return torch.mean(torch.square(self.f(
-            x) - y))  # Can also use torch.nn.functional.mse_loss(self.f(x), y) to possibly increase numberical stability
-
+        return torch.nn.functional.mse_loss(self.f(x), y)
 
 model = LinearRegressionModel()
 
 # Optimize: adjust W and b to minimize loss using stochastic gradient descent
 optimizer = torch.optim.SGD([model.b, model.W], 0.0001)
-for epoch in range(155000):
-    torch.nn.functional.mse_loss(x_train, y_train) # Compute loss gradients
+for epoch in range(150000):
+    model.loss(x_train, y_train).backward()  # Compute loss gradients
     optimizer.step()  # Perform optimization by adjusting W and b,
-    # similar to:
-    # model.W -= model.W.grad * 0.01
-    # model.b -= model.b.grad * 0.01
-
     optimizer.zero_grad()  # Clear gradients for next step
 
 # Print model variables and loss
